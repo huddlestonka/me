7d5dc4dd80d4ef12a93218e3323d9ba9f1e51087
huddlestonka <huddlestonka@gmail.com>
2020-05-16 03:48:14 -0700

    Added some nerdy quotes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 require('./mystyles.scss');
+import { allQuotes } from './MockData/Quotes.js';
+
+function changeQuote() {
+    let quotesDiv = document.getElementById('quotes');
+    quotesDiv.innerHTML = allQuotes[Math.floor(Math.random() * Math.floor(allQuotes.length))];
+    quotesDiv.setAttribute("style", "padding: 10px; max-width: 400px;");
+}
+
+window.onload = function() {
+    let quotesDiv = document.getElementById('quotes');
+    quotesDiv.innerHTML = allQuotes[Math.floor(Math.random() * Math.floor(allQuotes.length))];
+    quotesDiv.setAttribute("style", "padding: 10px; max-width: 400px;");
+}
+
+window.addEventListener("load", (event) => {
+    let quotesDiv = document.getElementById('quotes');
+    quotesDiv.addEventListener("click", changeQuote);
+});
