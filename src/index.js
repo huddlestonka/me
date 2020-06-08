cbae27e7492454a5d8eae82c4bcdf0413435e892
huddlestonka <huddlestonka@gmail.com>
2020-06-07 22:45:58 -0700

    Flex all the things

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import { allQuotes } from './MockData/Quotes.js';
 function changeQuote() {
     let quotesDiv = document.getElementById('quotes');
     quotesDiv.innerHTML = allQuotes[Math.floor(Math.random() * Math.floor(allQuotes.length))];
-    quotesDiv.setAttribute("style", "padding: 10px; max-width: 400px;");
 }
 
 window.onload = function() {
     let quotesDiv = document.getElementById('quotes');
     quotesDiv.innerHTML = allQuotes[Math.floor(Math.random() * Math.floor(allQuotes.length))];
-    quotesDiv.setAttribute("style", "padding: 10px; max-width: 400px;");
 }
 
 window.addEventListener("load", (event) => {
